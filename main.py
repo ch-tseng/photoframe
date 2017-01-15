03c93e37cd895fc6a44cca4b7b26e4fa7c07a42b
ch-tseng <ch.tseng@sunplusit.com>
2017-01-15 10:52:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-getFaceAfter = 3  #剛剛抓到的FACE可能晃動或使用者還未準備好，因此改為使用連續抓取後的第幾張FACE
+getFaceAfter = 99  #剛剛抓到的FACE可能晃動或使用者還未準備好，因此改為使用連續抓取後的第幾張FACE
 waitSeconds = 30  #若持續多久沒有動靜就進入預設的等待畫面
 LCD_size_w = 240
 LCD_size_h = 320
@@ -25,17 +25,17 @@ from gtts import gTTS
 from os.path import join, dirname
 from os import environ
 from watson_developer_cloud import VisualRecognitionV3
-visual_recognition = VisualRecognitionV3('2016-05-20', api_key='5cdc66469XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf71b')
+visual_recognition = VisualRecognitionV3('2016-05-20', api_key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 
 #PICamera setting
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (320, 240)
 camera.rotation = 90
 camera.video_stabilization = False
 camera.hflip = True
 camera.vflip = False
 camera.brightness = 60
-camera.ISO = 800
+#camera.ISO = 800
 
 #Import LCD 9341 libraries
 import Adafruit_ILI9341 as TFT
@@ -51,13 +51,15 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 
 #TFT LCD configuration and initial the LCD
-font = ImageFont.truetype('fonts/zh-TW.ttf', 30)
+font = ImageFont.truetype('fonts/WCL-07.ttf', 30)
 disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
 disp.begin()  # Initialize display.
 
 lastFaceDectedTime = time.clock()
 defaultDisplay = 0
 faceGetAccumulated = 0  #與getFaceAfter搭配，目前是第幾次抓到。
+
+screenRatio = camera.resolution[0] / 320.00
 
 #---------------------------------------------------------------------
 
@@ -99,15 +101,19 @@ def checkFace(imgfilePath, ynWatson = 0, useTTS = 0):
     img = cv2.imread(imgfilePath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
-#    face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt_tree.xml')
-    eye_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml')
+    #face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml')
+    #eye_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_mcs_eyepair_small.xml')
+    eye_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+    #eye_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml')
+    mouth_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_mcs_mouth.xml')
+    nose_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_mcs_nose.xml')
 
     faces = face_cascade.detectMultiScale(
         gray,
-        scaleFactor=1.2,
-        minNeighbors=4,
-        minSize=(20, 20)
+        scaleFactor=1.05,
+        minNeighbors=5,
+        minSize=(60, 60)
     )
 
     iFace = 0
@@ -116,19 +122,42 @@ def checkFace(imgfilePath, ynWatson = 0, useTTS = 0):
     if(len(faces)>0):
         lastFaceDectedTime = time.clock()
         for (x,y,w,h) in faces:
+            print('Image size w=%s, h=%s' % (img.shape[1], img.shape[0]))
             print('FACE: X=%s ,Y=%s, W=%s, H=%s  ---> X=%s, Y=%s' % (x,y,w,h,x+(w/2),y+(h/2)))
             if iFace == 0: 
                 xFace = x+(w/2)
                 yFace = y+(h/2)
 
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,246),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
             iFace += 1
+
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex,ey,ew,eh) in eyes:
                 print('EYE: X=%s ,Y=%s, W=%s, H=%s' % (ex,ey,ew,eh))
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(251,34,12),2)
+
+            mouth = mouth_cascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=1.05,
+                minNeighbors=5,
+                minSize=(60, 60))
+            for (mx,my,mw,mh) in mouth:
+                print('Mouth: X=%s ,Y=%s, W=%s, H=%s' % (mx,my,mw,mh))
+                cv2.rectangle(roi_color,(mx+int(mw/5),my+int(mh/2+(mh/5))),(mx+(mw-int(mw/5)),my+int(mh/2+mh/4)),(4,162,252),2)
+                #cv2.rectangle(roi_color,(mx,my),(mx+mw,my+int(mh/2)),(4,162,252),2)
+
+            nose = nose_cascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=1.05,
+                minNeighbors=5,
+                minSize=(60, 60))
+            for (nx,ny,nw,nh) in nose:
+                print('Nose: X=%s ,Y=%s, W=%s, H=%s' % (nx,ny,nw,nh))
+                cv2.rectangle(roi_color,(nx+int((nw/2)-(nw/5)),ny-int(nh/3)),(nx+int((nw/2)+(nw/4)),ny+int(nh*2/3)),(21,221,30),2)
+            #    cv2.rectangle(roi_color,(nx,ny),(nx+int(nw/2),ny+nh),(21,221,30),2)
+
 
             cv2.imwrite("cv2.jpg", img)
             defaultDisplay = 0
@@ -139,7 +168,7 @@ def checkFace(imgfilePath, ynWatson = 0, useTTS = 0):
             faceGetAccumulated += 1
             print('faceGetAccumulated=%s' % (faceGetAccumulated))
 
-            if (faceGetAccumulated>=getFaceAfter and ynWatson==1 and xFace<460 and xFace>180 and yFace<360 and yFace>160):  #Use Watson to analysis if the first face is at center (160<x<170)
+            if (faceGetAccumulated>=getFaceAfter and ynWatson==1 and xFace<(320*screenRatio) and xFace>(90*screenRatio) and yFace<(180*screenRatio) and yFace>(80*screenRatio)):  #Use Watson to analysis if the first face is at center (160<x<170)
                 faceGetAccumulated = 0
                 os.system('omxplayer --no-osd voice/face1.mp3')
                 with open(join(dirname(__file__), imgfilePath), 'rb') as image_file:
@@ -265,7 +294,7 @@ def checkFace(imgfilePath, ynWatson = 0, useTTS = 0):
                                     tts.save("name.mp3")
                                     os.system('omxplayer --no-osd name.mp3')
 
-                        time.sleep(2)
+                        #time.sleep(2)
 
     else:
         if time.clock() - lastFaceDectedTime >  waitSeconds:
@@ -275,4 +304,8 @@ displayDefaultImg()
 
 while True:
     camera.capture('camera.jpg')
-    checkFace("camera.jpg",1,1)
+
+    try:
+        checkFace("camera.jpg",1,1)
+    except:
+        draw_rotated_text(disp.buffer, "糟糕, 程式發生錯誤了!", (10, 10), 180, font, fill=(2,2,252))
